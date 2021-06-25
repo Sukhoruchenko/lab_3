f7a6ba43e7c62c224603d0db981d2b910ade583f
Xenia <ksenia_s.a@bk.ru>
2021-06-25 17:29:01 +0700

    Изменение формы

--- Form.cpp
+++ Form.cpp
@@ -3,35 +3,37 @@
 
 Form::~Form()
 {
-    delete c;
     delete ui;
-    delete model;
+    delete fModel;
+    delete ObservationAdapter;
     delete FileModel;
+    delete pie;
+    delete bar;
+    delete folder;
+    delete type;
 }
 
-Form::Form(QMainWindow *parent) : QMainWindow(parent), ui(new Ui::Form)
+Form::Form(QMainWindow *parent) : QMainWindow(parent), ui(new Ui::Form),folder(new GroupingByFolder()),type(new GroupingByType()), group(folder)
 {
     ui->setupUi(this);
 
     QString homePath = QDir::homePath();
+    fModel =  new QFileSystemModel(this);
+    fModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
+    fModel->setRootPath(homePath);
 
-    model =  new QFileSystemModel(this);
-    FileModel = new BrowserModel();
+    ObservationAdapter = new Observation(ui->stackedWidget_7->layout());
+    bar = new BarDrawing(ui->stackedWidget_7->layout());
+    pie = new PieDrawing(ui->stackedWidget_7->layout());
+    View = ObservationAdapter;
+    group->Fix(View);
 
-    model->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
-    model->setRootPath(homePath);
+    ui->treeView_7->setModel(fModel);
+    ui->treeView_7->header()->setSectionResizeMode(QHeaderView::ResizeToContents);
 
-    ui->treeView->setModel(model);
-    ui->tableView->setModel(FileModel);
-
-    ui->treeView->header()->setSectionResizeMode(QHeaderView::ResizeToContents);
-    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
-
-    connect(ui->comboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &Form::selectStrategy);
-    connect(ui->treeView->selectionModel(), &QItemSelectionModel::selectionChanged, this,  &Form::selectingModifiedSlot);
-
-    c = new Context();
-    strategy = Strategies::GroupingFolder;
+    connect(ui->comboBox_14, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &Form::selecting);
+    connect(ui->comboBox_13, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &Form::selectStrategy);
+    connect(ui->treeView_7->selectionModel(), &QItemSelectionModel::selectionChanged, this,  &Form::selectingModifiedSlot);
 }
 
 void Form::selectStrategy(int index)
@@ -39,23 +41,14 @@ void Form::selectStrategy(int index)
     switch (index)
     {
     case 0:
-        strategy = Strategies::GroupingFolder;
+        group = folder;
         break;
     case 1:
-        strategy = Strategies::GroupingType;
+        group = type;
         break;
     }
-
-    if (strategy == Strategies::GroupingType)
-        c->setStrategy(new GroupingByType);
-    else
-       c->setStrategy(new GroupingByFolder);
-
-    if (path.isEmpty())
-        return;
-
-    data = c->explore(path);
-    FileModel->setModel(data);
+    group->Fix(View);
+    group->explore(path);
 }
 
 void Form::selectingModifiedSlot(const QItemSelection &selected, const QItemSelection &deselected)
@@ -63,17 +56,27 @@ void Form::selectingModifiedSlot(const QItemSelection &selected, const QItemSele
     Q_UNUSED(deselected);
 
     QModelIndexList indexes = selected.indexes();
-    path = model->filePath(indexes[0]);
+    path = fModel->filePath(indexes[0]);
+    group->explore(path);
+}
 
-    if (strategy == Strategies::GroupingType)
-        c->setStrategy(new GroupingByType);
-    else
-        c->setStrategy(new GroupingByFolder);
 
-    if (path.isEmpty())
-        return;
-
-    data = c->explore(path);
-    FileModel->setModel(data);
+void Form::selecting(int index)
+{
+    switch(index)
+    {
+        case 0:
+            View = ObservationAdapter;
+            break;
+        case 1:
+            View = pie;
+            break;
+        case 2:
+            View = bar;
+            break;
+    }
+    group->Fix(View);
+    group->explore(path);
+    ui->stackedWidget_7->setCurrentIndex(index);
 }
 
--- Form.h
+++ Form.h
@@ -1,9 +1,16 @@
 #ifndef FORM_H
 #define FORM_H
+#include <QDebug>
+#include <memory>
 #include <QWidget>
 #include "Context.h"
+#include "Drawing.h"
+#include "Drawing.h"
 #include <QMainWindow>
+#include "Observation.h"
+#include <QItemSelection>
 #include "BrowserModel.h"
+#include <QFileSystemModel>
 #include "GroupingByType.h"
 #include "GroupingByFolder.h"
 
@@ -16,23 +23,25 @@ class Form : public QMainWindow
 {
     Q_OBJECT
 public:
-    enum Strategies
-    {
-        GroupingFolder,
-        GroupingType
-    };
-    ~Form();
+public:
     explicit Form(QMainWindow *parent = nullptr);
+    ~Form();
 private:
     Ui::Form *ui;
-    QFileSystemModel* model;
+    QFileSystemModel* fModel;
     BrowserModel* FileModel;
-    Strategies strategy;
-    Context* c;
     QString path;
-    QList<Data> data;
+    Browser* folder;
+    Browser* type;
+    Browser* group;
+    FileObserver* View;
+    FileObserver* ObservationAdapter;
+    FileObserver* pie;
+    FileObserver* bar;
 public slots:
     void selectStrategy(int index);
     void selectingModifiedSlot(const QItemSelection& selected, const QItemSelection& deselected);
+    void selecting(int index);
 };
 #endif // FORM_H
+
