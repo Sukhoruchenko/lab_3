8338b3543086ea72e29770bb2fb7e99242659e56
Xenia <ksenia_s.a@bk.ru>
2021-06-25 13:05:06 +0700

    Изменен файл FileExplorer.h

--- FileExplorer.h
+++ FileExplorer.h
@@ -1,15 +1,37 @@
 #ifndef FILEEXPLORER_H
 #define FILEEXPLORER_H
 #include <QDir>
+#include "Data.h"
+#include <QString>
+#include <QVector>
 #include <QFileInfo>
 #include <QTextStream>
-#include "SomeData.h"
 
-class IExplorer
+class FileObserver
 {
 public:
-    virtual QList<Data> explore(const QString& path) = 0;
-    virtual ~IExplorer() {}
+    FileObserver() {}
+    virtual void Update(QList<Data> d) const = 0;
+    virtual ~FileObserver() = default;
+};
+
+class Browser
+{
+private:
+    FileObserver* observer;
+public:
+    virtual ~Browser() {}
+    virtual void explore(const QString& path) = 0;
+
+    void Fix(FileObserver* o)
+    {
+        if (o) observer = o;
+    }
+
+    void Finish(const QList<Data>& d) const
+    {
+        observer->Update(d);
+    }
 };
 
 #endif // FILEEXPLORER_H
